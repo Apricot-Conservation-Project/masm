50f9a62be738369d33a647a9a5440874b930d129
bendn <bend.n@outlook.com>
2024-04-16 23:10:21 +0700

    smol changes

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo "Building"
 cargo build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target wasm32-unknown-unknown --release --lib
 echo "Binding"
-wasm-bindgen ~/.target/wasm32-unknown-unknown/release/masm.wasm --out-dir pkg/ --no-typescript --target web
+wasm-bindgen /mnt/HDD500/.target/wasm32-unknown-unknown/release/masm.wasm --out-dir pkg/ --no-typescript --target web
 echo "Opting"
 wasm-opt -O4 pkg/masm_bg.wasm -o tmp.wasm
 echo "Compressing"
